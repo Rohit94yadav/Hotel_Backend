8f9ee8d54719d50f9ad0e93c96842b8ae5d647f6
poojakittu <poojayadav.yadav173@gmail.com>
2023-02-20 14:24:41 +0530

    added

--- Model/Hotel.model.js
+++ Model/Hotel.model.js
@@ -2,23 +2,23 @@ const { mongoose } = require("mongoose");
 
 const hotelSchema = mongoose.Schema(
   {
-    hotelName: { type: String, required: true, unique: true },
-    image_url: [{ type: String, required: true }],
-    email: { type: String, required: true },
-    phoneNumber: { type: Number, required: true },
-    address: { type: String, required: true },
-    city: { type: String, required: true },
-    pinCode: { type: Number, required: true },
-    rating: { type: Number, required: true },
-    facilites: { type: String, required: true },
-    ownerName: { type: String,required: true  },
+    hotelName: { type: String,  unique: true },
+    image_url: [{ type: String, }],
+    email: { type: String, },
+    phoneNumber: { type: Number, },
+    address: { type: String, },
+    city: { type: String, },
+    pinCode: { type: Number, },
+    rating: { type: Number, },
+    facilites: { type: String, },
+    ownerName: { type: String, },
     contactName: { type: String },
     isApprovedByAdmin:{type:String, default:"false"},
     allRooms: [
       {
-        roomType: { type: String, required: true },
-        numberofRooms: { type: Number, required: true },
-        priceperNight: { type: Number, required: true },
+        roomType: { type: String, },
+        numberofRooms: { type: Number, },
+        priceperNight: { type: Number, },
       },
     ]
   },
