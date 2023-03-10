8ee54a1c5b9fc0daf98ab30145c335c87f8435ec
poojakittu <poojayadav.yadav173@gmail.com>
2023-03-10 20:49:40 +0530

    added

--- Model/TourTravel.Model.js
+++ Model/TourTravel.Model.js
@@ -21,7 +21,7 @@ const TourTravelSchema = mongoose.Schema(
     ],
     userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "agent",
+      ref: "admin",
       required: true,
     },
   },
