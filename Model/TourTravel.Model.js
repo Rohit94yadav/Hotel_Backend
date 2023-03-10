f00fda4d816ab0c3ccb3eec2349c7b1acf16d0ef
poojakittu <poojayadav.yadav173@gmail.com>
2023-03-10 20:40:38 +0530

    added

--- Model/TourTravel.Model.js
+++ Model/TourTravel.Model.js
@@ -2,7 +2,6 @@ const { mongoose } = require("mongoose");
 
 const TourTravelSchema = mongoose.Schema(
   {
-    
     name: { type: String },
     image_url: [{ type: String }],
     contact: { type: Number },
