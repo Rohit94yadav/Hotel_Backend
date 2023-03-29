9ae68b48bc74372fc171b4366d24d0b95a00fe66
poojakittu <poojayadav.yadav173@gmail.com>
2023-03-29 18:25:08 +0530

    added

--- middleware/authenticate.Agent.js
+++ middleware/authenticate.Agent.js
@@ -1,23 +1,23 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config()
+require("dotenv").config();
 const authenticateAgent = (req, res, next) => {
-  const token=req.headers.authorization;
- 
-  if(!token){
-    res.send({msg:"Please login first"})
-  }
+  const token = req.headers.authorization;
 
-  const decoded=jwt.verify(token, process.env.key);
-  
-  if(decoded){
-   
-      req.body.AgentId=decoded.AgentId;
-      next()
-  }else{
-      res.send({msg:"Please login first"})
+  if (!token) {
+    res.send({ msg: "Please login first" });
+  }
+  console.log(token);
+
+  const decoded = jwt.verify(token, process.env.key);
+
+  if (decoded) {
+    req.body.AgentId = decoded.AgentId;
+    next();
+  } else {
+    res.send({ msg: "Please login first" });
   }
 };
 
 module.exports = {
-    authenticateAgent
+  authenticateAgent,
 };
