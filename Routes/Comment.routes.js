1d0e704925e57b168a0621fa1b5ceb731eb3dfd7
poojakittu <poojayadav.yadav173@gmail.com>
2023-03-30 08:40:07 +0530

    added

--- Routes/Comment.routes.js
+++ Routes/Comment.routes.js
@@ -6,12 +6,31 @@ const { UserModel } = require("../Model/User.model");
 const CommentRoutes = express.Router();
 const jwt = require("jsonwebtoken");
 
-CommentRoutes.get("/productcommment/:id", async (req, res) => {
+CommentRoutes.get("/", async (req, res) => {
+  const token = req.headers.authorization;
+  const decoded = jwt.verify(token, process.env.key);
+  const x = decoded.userId;
+  console.log(x);
+
+  try {
+    const product = await CommentModel.find({ userId: x });
+    console.log(product);
+    res.send({ data: product, total: product.length });
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).send({
+      error: true,
+      msg: "something went wrong",
+    });
+  }
+});
+
+CommentRoutes.get("/hotelcomment/:id", async (req, res) => {
   const payload = req.params.id;
   try {
-    const product = await CommentModel.find({hotelId:payload});
+    const product = await CommentModel.find({ hotelId: payload });
     console.log(product);
-    res.send({ data: product,total:product.length });
+    res.send({ data: product, total: product.length });
   } catch (error) {
     console.log("error", error);
     res.status(500).send({
@@ -53,7 +72,7 @@ CommentRoutes.post("/add", authenticate, async (req, res) => {
 CommentRoutes.get("/:id", async (req, res) => {
   const payload = req.params.id;
   try {
-    const product = await CommentModel.find({ postId: payload });
+    const product = await CommentModel.find({ _id: payload });
     console.log(product);
     res.send({ data: product });
   } catch (error) {
@@ -62,6 +81,48 @@ CommentRoutes.get("/:id", async (req, res) => {
       error: true,
       msg: "something went wrong",
     });
+  }
+});
+
+CommentRoutes.patch("/update/:id", authenticate, async (req, res) => {
+  const Id = req.params.id;
+  const note = await CommentModel.findOne({ _id: Id });
+  const token = req.headers.authorization;
+  const decoded = jwt.verify(token, process.env.key);
+  const x = JSON.stringify(note.userId);
+  const y = JSON.stringify(decoded.userId);
+
+  try {
+    if (y !== x) {
+      res.send({ msg: "You are not Recognized" });
+    } else {
+      await CommentModel.findByIdAndUpdate({_id:Id},req.body);
+      res.send({ msg: "update comment" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send({ msg: "Something went wrong" });
+  }
+});
+
+CommentRoutes.patch("/delete/:id", authenticate, async (req, res) => {
+  const Id = req.params.id;
+  const note = await CommentModel.findOne({ _id: Id });
+  const token = req.headers.authorization;
+  const decoded = jwt.verify(token, process.env.key);
+  const x = JSON.stringify(note.userId);
+  const y = JSON.stringify(decoded.userId);
+
+  try {
+    if (y !== x) {
+      res.send({ msg: "You are not Recognized" });
+    } else {
+      await CommentModel.findOneAndDelete({_id:Id});
+      res.send({ msg: "update comment" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send({ msg: "Something went wrong" });
   }
 });
 
